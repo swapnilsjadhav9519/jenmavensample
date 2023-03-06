d4e66352fc0de1386bcb91190730f2cddde81028
unknown <swapnil9519@gmail.com>
2023-03-06 09:55:09 +0530

    maven project add

---
+++ src/test/java/com/assignment/my_assignment/AppTest.java
@@ -0,0 +1,13 @@
+package com.assignment.my_assignment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
